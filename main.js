216b98b4226b66835c1aa943450993e408795c0e
boy1434@naver.com <boy1434@naver.com>
2024-07-09 23:22:46 +0900

    rr

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_KEY = `50b049f2fbce4d8f94a2df193555ac3d`;
 
 const getLatestNews = async () => {
     const url = new URL(`
-            http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines
+            https://jade-marshmallow-4f5564.netlify.app//top-headlines
         `);
         const response = await fetch(url);
         const data = await response.json();
